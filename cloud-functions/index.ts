20ccc409785e73904c48ba4424296c818b00ade1
paulStockrx <paul@stockrx.uk>
2023-03-11 13:10:18 +0200

    integrated chatgpt calls

--- cloud-functions/index.ts
+++ cloud-functions/index.ts
@@ -1,20 +1,42 @@
+import { Configuration, OpenAIApi } from "openai";
+
 export class CodeCommenter {
+    private readonly configuration: Configuration;
 
     constructor() {
-      console.log("Constructor called!")
+        this.configuration = new Configuration({
+            apiKey: process.env.OPENAI_API_KEY,
+        });
+        console.log("Constructor called!");
     }
-  
+
     /**
-    * Method that returns a comment for a snippet of code.
-    */
-    comment() {
-      return "Hello world!";
+     * Method that returns a comment for a snippet of code.
+     */
+    async comment() {
+        const openai = new OpenAIApi(this.configuration);
+
+        const result = await openai.createChatCompletion({
+            model: "gpt-3.5-turbo",
+            messages: [
+                {
+                    role: "user",
+                    content: "Give me 5 fun party ideas",
+                },
+            ],
+        });
+
+        const returnedMsg = result.data.choices[0].message;
+
+        console.log(returnedMsg);
+
+        return returnedMsg;
     }
-  
+
     /**
      * Method that returns a markdown documentation for a snippet of code
      */
     document() {
-      return "Hello world!";
+        return "Hello world!";
     }
-  }
+}
