e8fd08f2d88b41679a80a28449283a9f8cf645eb
paulStockrx <paul@stockrx.uk>
2023-03-11 11:52:27 +0200

    Added starter for cloud functions

---
+++ cloud-functions/index.ts
@@ -0,0 +1,20 @@
+export class CodeCommenter {
+
+    constructor() {
+      console.log("Constructor called!")
+    }
+  
+    /**
+    * Method that returns a comment for a snippet of code.
+    */
+    comment() {
+      return "Hello world!";
+    }
+  
+    /**
+     * Method that returns a markdown documentation for a snippet of code
+     */
+    document() {
+      return "Hello world!";
+    }
+  }
