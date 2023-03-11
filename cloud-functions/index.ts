b5cf9e2ecde4e241bb42e783b4fd43e7db505191
paulStockrx <paul@stockrx.uk>
2023-03-11 19:59:28 +0200

    added cde markdown documentation tool

--- cloud-functions/index.ts
+++ cloud-functions/index.ts
@@ -13,7 +13,8 @@ interface HTTPRequest {
 
 export class HttpServer {
     private readonly configuration: Configuration;
-    private readonly startingPrompt: string;
+    private readonly startingPromptSnippet: string;
+    private readonly startingPromptDocumentation: string;
 
     constructor() {
         this.configuration = new Configuration({
@@ -21,12 +22,19 @@ export class HttpServer {
         });
         // this.startingPrompt = "Your task is to comment typescript code. You will receive just the snipper of code from now on, and you will place inline comments only where necessary to document the function. Do not add a comment on every line, only add comments where necessary, and make them inline. If the snippet of code is a function or a class definition, then you will also prepend the function with a JSDOC comment of the code.";
         
-        this.startingPrompt = `You are now an exceptional code documentation tool created by the best programmers around the world. You are specialized in typescript and can understand even the context of the code provided. From now on, your task is to comment on incoming code snippets. 
+        this.startingPromptSnippet = `You are now an exceptional code documentation tool created by the best programmers around the world. You are specialized in typescript and can understand even the context of the code provided. From now on, your task is to comment on incoming code snippets. 
         You will receive just the snippet of code and place inline comments only where necessary to document the function. Make sure your comments are readable and consider the whole snippet context, not just the explicit line, so just by reading your comments anyone can understand what the code is doing. Ignore console logs and try to be succinct but don’t miss important details.
         Do not comment on every line, only add comments where necessary, and make them inline. 
         Remember, do not comment on every line, only add comments where necessary! Otherwise, you will make me sad. 
         Try to use as little comments as possible but when neccesary provide enough details.
         Very important, if the code snippet is a function or a class definition, then you will also prepend the function with a JSDOC comment of the code!
+        `
+
+        this.startingPromptDocumentation = `
+        You are now an exceptional code documentation tool created by the best programmers around the world. You are specialized in typescript and can understand even the context of the code provided. From now on, your task is to generate markdown documentation for the following typescript snippet of code.  
+        Please note that the markdown should have the title of the function or class and should only have the following sections:
+        signature, arguments, description, and notes.
+        Headers should each have a capital letter, it should say if it's a function or class in the title.
         `
         
         console.log("Constructor called!");
@@ -35,7 +43,7 @@ export class HttpServer {
     async handleCodeSnippet(request: HTTPRequest) {
         const openai = new OpenAIApi(this.configuration);
 
-        const startingPrompt = this.startingPrompt;
+        const startingPrompt = this.startingPromptSnippet;
 
         const codeSnippet = request.body.content;
 
@@ -63,4 +71,36 @@ export class HttpServer {
             statusCode: 200,
         };
     }
+
+    async handleDocumentation(request: HTTPRequest) {
+      const openai = new OpenAIApi(this.configuration);
+
+      const startingPrompt = this.startingPromptDocumentation;
+
+      const codeSnippet = request.body.content;
+
+      const result = await openai.createChatCompletion({
+          model: "gpt-3.5-turbo",
+          messages: [
+              {
+                  role: "user",
+                  content: startingPrompt,
+              },
+              {
+                  role: "user",
+                  content: codeSnippet,
+              },
+          ],
+      });
+
+      const returnedMsg = result.data.choices[0].message;
+
+      return {
+          body: {
+            content: returnedMsg?.content ?? 'ERROR'
+          },
+          headers: { "content-type": "application/json" },
+          statusCode: 200,
+      };
+  }
 }
