826d552087319bb50e80c38431639951adbc8351
Aleks <alexandruvlad.adam@gmail.com>
2023-03-11 15:28:10 +0200

    Updated extension with comment code command

--- chatgpt-documentation-assistant/src/extension.ts
+++ chatgpt-documentation-assistant/src/extension.ts
@@ -6,21 +6,33 @@ import * as vscode from 'vscode';
 // Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "chatgpt-documentation-assistant" is now active!');
-
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('chatgpt-documentation-assistant.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from chatgpt-documentation-assistant!');
+	let disposable = vscode.commands.registerTextEditorCommand('chatgpt-documentation-assistant.commentCode', async (editor, edit) => {
+		// Get selected code
+
+		if (!editor) {
+			// Nothing to do
+			return;
+		}
+
+		const selection = editor.selection;
+
+		if (!selection || selection.isEmpty) {
+			// Nothing to do
+			return;
+		}
+		
+		const selectionRange = new vscode.Range(selection.start.line, selection.start.character, selection.end.line, selection.end.character);
+		const highlighted = editor.document.getText(selectionRange);
+
+		// TODO: implement API endpoint call to replace the code with the commented code
+
 	});
 
 	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() { }
