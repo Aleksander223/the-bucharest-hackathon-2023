a952eb8347a520b9313fadca5f212ac6db1927e0
Adam Alexandru <alexandruvlad.adam@gmail.com>
2023-03-11 22:28:36 +0200

    Updated snippet prompt

--- cloud-functions/index.ts
+++ cloud-functions/index.ts
@@ -22,12 +22,11 @@ export class HttpServer {
         });
         // this.startingPrompt = "Your task is to comment typescript code. You will receive just the snipper of code from now on, and you will place inline comments only where necessary to document the function. Do not add a comment on every line, only add comments where necessary, and make them inline. If the snippet of code is a function or a class definition, then you will also prepend the function with a JSDOC comment of the code.";
         
-        this.startingPromptSnippet = `You are now an exceptional code documentation tool created by the best programmers around the world. You are specialized in typescript and can understand even the context of the code provided. From now on, your task is to comment on incoming code snippets. 
-        You will receive just the snippet of code and place inline comments only where necessary to document the function. Make sure your comments are readable and consider the whole snippet context, not just the explicit line, so just by reading your comments anyone can understand what the code is doing. Ignore console logs and try to be succinct but don’t miss important details.
+        this.startingPromptSnippet = `You are now a cutting-edge code documentation tool. You are specialized in typescript and can understand even the context of the code provided. From now on, your task is to comment incoming code snippets. 
+        You will receive just the snippet of code and place inline comments only where necessary to document the function. Make sure your comments are readable and consider the whole snippet context, not just the explicit line, so just by reading your comments anyone can understand what the code is doing. Ignore console logs and try to be succinct but don't miss important details.
         Do not comment on every line, only add comments where necessary, and make them inline. 
-        Remember, do not comment on every line, only add comments where necessary! Otherwise, you will make me sad. 
-        Try to use as little comments as possible but when neccesary provide enough details.
-        Very important, if the code snippet is a function or a class definition, then you will also prepend the function with a JSDOC comment of the code!
+        Please add JSDOC comments if the provided code snippet is a valid function or class, otherwise (for instance if the provided code snippet is not a function or class definition) then do not add the JSDOC comment.
+        You will keep the same formatting of the source code as the input. This is mandatory.
         `
 
         this.startingPromptDocumentation = `
